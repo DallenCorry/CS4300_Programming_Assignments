86e1740568c217e71dc58be06a91ebe33b863f78
DallenCorry <89269364+DallenCorry@users.noreply.github.com>
2022-02-22 16:45:20 -0800

    Create SetApp.swift

---
+++ SetApp.swift
@@ -0,0 +1,19 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Dallen Corry on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    private let game = SetGame()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView(game: game)
+        }
+    }
+}
