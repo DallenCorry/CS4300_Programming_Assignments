66955a993842cc1b33e81c7033dcc007912655b6
DallenCorry <89269364+DallenCorry@users.noreply.github.com>
2022-03-07 13:34:32 -0700

    Update SetGame.swift

--- SetGame.swift
+++ SetGame.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 class SetGame: ObservableObject {
+    var tempBool = false
     //here is where I should set the shapes(Content?) of my card, with their colors, numbers etc.
     @Published var model = createModel()
     
@@ -33,8 +34,13 @@ class SetGame: ObservableObject {
     }
     
     func addCards(_ number:Int) {
-//        let num = model.numberOfCardsOnScreen
+        tempBool.toggle()
         model = GameModel(cardsArray: cards, numberOfCardsOnScreen: 10)
-       
+        model.cardsOnScreen.indices.forEach { model.cardsOnScreen[$0].isSelected = false }//this needs to change if I want to keep the selection when the button is pushed. (Because right now it is being set to empty each time new cards are added.)
+//        if (tempBool){
+//            model = GameModel(cardsArray: cards, numberOfCardsOnScreen: model.numberOfCardsOnScreen - 1)
+//        } else {
+//            model = GameModel(cardsArray: cards, numberOfCardsOnScreen: model.numberOfCardsOnScreen + 1)
+//        }
     }
 }
