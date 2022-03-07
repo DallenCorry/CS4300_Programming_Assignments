e1adf0049c98b049ad3b19a107d0e8879dd8a537
DallenCorry <89269364+DallenCorry@users.noreply.github.com>
2022-03-07 13:33:48 -0700

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -45,42 +45,22 @@ struct ContentView: View {
     }
 }
 
-//struct DiamondForEachable: Identifiable {
-//    let id = UUID()
-//    static func diamonds(with count: Int) -> [DiamondForEachable] {
-//        .init(repeating: .init(), count: count)
-//    }
-//}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//TODO:
+//TODO: -
 //[ ] 1. Make solo Set
         //[X] 2. Keep all shapes good size
         //[X] 3. Same aspect ratio throughout
         //[X] 4. Symbols should be proportional to card
-//[ ] 5. Select 3 cards, Must be visible and obvious which one. (change color of border?)
-//[ ] 6. Indicate if match or not. Cards must look different than just normal, and than just selected
-//[ ] 7. Support "deselection" (only if 1 or 2 cards selected)
+        //[X] 5. Select 3 cards, Must be visible and obvious which one. (change color of border?)
+//[ ] 6. Indicate if match or not. Cards must look different than just normal, and different than just selected
+        //[X] 7. Support "deselection" (only if 1 or 2 cards selected)
 //[ ] 8. a) On a match: replace 3 cards with 3 new ones.
 //[ ] 8. b) adjust view when there are no more cards to draw
 //[ ] 8. c) if tapped card not part of set, select it
 //[ ] 8. d) if tapped card was part of set, do nothing (don't deselect the good set, and don't select anything else)
-//[ ] 9. if the 3 cards are not a match, deselect the other 3 and select the 4th (even if it was one of the 3)
+        //[X] 9. if the 3 cards are not a match, deselect the other 3 and select the 4th (even if it was one of the 3)
 //[ ] 10. a) deal 3 button must replace a set of 3 if possible
-//[ ] 10. b) deal 3 button must add 3 new cards if 0,1,2, or 3 cards selected (if no set)
+//[ ] 10. b) deal 3 button must add 3 new cards if 0,1,2, or 3 cards selected (if no set) and NOT affect the selection
 //[ ] 10. c) deal 3 button must be disabled if no more cards.
 //[ ] 11. New game button starts a new game
         //[X] 12. Squiggle can be a rectangle
@@ -90,6 +70,8 @@ struct ContentView: View {
         //[X] 16. must use enum
 //[ ] 17. must use a closure
 //[ ] 18. should work in portrait and landscape
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let game = SetGame()
