41e6158127345c9d316978c017f9297205d510ee
DallenCorry <89269364+DallenCorry@users.noreply.github.com>
2022-04-13 15:07:29 -0600

    edited, from computer

---
+++ Assigment6/MemorizePart2 copy/MemorizePart2/Pie.swift
@@ -0,0 +1,47 @@
+//
+//  Pie.swift
+//  MemorizePart2
+//
+//  Created by Dallen Corry on 2/14/22.
+//
+
+import SwiftUI
+
+struct Pie: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockWise: Bool = false
+    
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(startAngle.radians, endAngle.radians)
+        }
+        set {
+            startAngle = Angle.radians(newValue.first)
+            endAngle = Angle.radians(newValue.second)
+        }
+    }
+
+    
+    func path(in rect: CGRect) -> Path {
+        let center = CGPoint(x: rect.midX, y:rect.midY)
+        let radius = min(rect.width, rect.height)/2
+        let start = CGPoint(
+            x:  center.x+radius * CGFloat(cos(startAngle.radians)),
+            y:center.x+radius * CGFloat(sin(startAngle.radians))
+        )
+        
+        var p = Path()
+        p.move(to: center)
+        p.addLine(to: start)
+        p.addArc(
+            center: center,
+            radius: radius,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: !clockWise
+        )
+        p.closeSubpath()
+        return p
+    }
+}
